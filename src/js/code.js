2bec87e30bfb7bba6bc7fd0bce2371e51ac21ca9
Phann020126 <daniel020126@gmail.com>
2023-12-25 16:11:04 -0500

    change-platform

--- src/js/code.js
+++ src/js/code.js
@@ -1,15 +1,12 @@
-function choosePlatform(parameter)
+function choosePlatform(idPlatform)
 {
-    switch(parameter)
+    var platform = document.getElementById("platform");
+    var platforms = platform.querySelectorAll("a");
+
+    for(i=0; i<platforms.length; i++)
     {
-        case "Party":
-            // code block
-            break;
-        case "Match":            
-            // code block
-            break;
-        case "Sream":
-            // code block
-            break;
+        platforms[i].removeAttribute("selected");
     }
+
+    platforms[idPlatform].setAttribute("selected", "True");
 }
